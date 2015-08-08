cd5834a26e7605637f146fa5d6c35af35402e541
Ed Salinas <edsalinas7b@yahoo.com>
2015-08-09 06:20:45 +0800

    added perks to web client

--- web_client/app.js
+++ web_client/app.js
@@ -8,12 +8,31 @@ $( "#login" ).on( "click",  function() {
      },
      success: function(data, status, xhr) {
       if(data.data.user_type)
-        window.location='supplier.html';
+        window.location='supplier.html?id='+data.data.id;
       else
-        window.location='buyer.html';
+        window.location='buyer.html?id='+data.data.id;
      },
     error: function (request, textStatus, errorThrown) {
       alert("Invalid Login");
     }
   });
 });
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var id = getUrlParameter('id');
+if(id!=undefined)
+  alert(id);
