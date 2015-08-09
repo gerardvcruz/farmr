0c711245c9b54026a32c374bbf6af43bc596b5bf
Gerard V. Cruz <gerardcruz@live.com>
2015-08-09 13:34:56 +0800

    orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,6 +2,11 @@ module Api
   module V1
     class OrdersController < ResourceController
 
+      private
+        def orders_params
+          params.require(:order).permit!
+        end
+      end
     end
   end
 end
