d74a50945a6d85801a39c09a1761b2ec6fabb8c5
Gerard V. Cruz <gerardcruz@live.com>
2015-08-09 12:04:42 +0800

    remove unneccesary folders, orders per user, unit views

--- app/controllers/api/v1/supplies_controller.rb
+++ app/controllers/api/v1/supplies_controller.rb
@@ -2,10 +2,10 @@ module Api
   module V1
     class SuppliesController < ResourceController
 
-    private
-      def supply_params
-        params.require(:supply).permit!
+      private
+        def supply_params
+          params.require(:supply).permit!
+        end
       end
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
   include DeviseTokenAuth::Concerns::User
+
   has_many :supplies, foreign_key: "supplier_id"
   has_many :categories, through: :supplies
+  has_many :buyer_orders, class_name: "Order", foreign_key: "buyer_id"
+  has_many :supplier_orders, class_name: "Order", foreign_key: "supplier_id"
 end
